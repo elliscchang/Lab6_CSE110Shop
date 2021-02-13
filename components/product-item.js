e484a0cf2ba511436e7e80efcf733e3e1e848064
elliscchang <ecc013@ucsd.edu>
2021-02-12 19:20:59 -0800

    created js files to get shop working

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,83 @@
 // product-item.js
+var count = 0;
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(item) {
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+
+    // create css elemets to apply to the shadow dom
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', './styles/styles.css');
+    shadow.appendChild(linkElem);
+
+    // create html elements
+    const product = document.createElement('li');
+    product.setAttribute('class', 'product');
+    shadow.appendChild(product);
+
+    const image = document.createElement('img');
+    image.setAttribute('src', item.image);
+    image.setAttribute('alt', item.title);
+    image.setAttribute('width', 200);
+    product.appendChild(image);
+
+    const title = document.createElement('p');
+    title.setAttribute('class', 'title');
+    title.textContent = item.title;
+    product.appendChild(title);
+
+    const price = document.createElement('p');
+    price.setAttribute('class', 'price');
+    price.textContent = '$' + item.price;
+    product.appendChild(price);
+
+    const button = document.createElement('button');
+    button.textContent = "Add to Cart";
+    product.appendChild(button);
+
+    console.log(localStorage.getItem(item.id.toString()));
+    if (localStorage.getItem(item.id.toString()) === null) {
+      localStorage.setItem(item.id.toString(), '1');
+      button.setAttribute("onclick", "alert('Added to Cart!')");
+      button.textContent = "Add to Cart";
+      product.appendChild(button);
+    }
+    else if (localStorage.getItem(item.id.toString()) === '1') {
+      button.setAttribute("onclick", "alert('Added to Cart!')");
+      button.textContent = "Add to Cart";
+      product.appendChild(button);
+    }
+    else {
+      button.setAttribute("onclick", "alert('Removed from Cart!')");
+      button.textContent = "Remove from Cart";
+      product.appendChild(button);
+      count++;
+      document.getElementById('cart-count').textContent = count;
+    }
+
+    button.onclick = function (event) {
+      if (event.target.innerHTML == "Add to Cart") {
+        alert('Added to Cart!');
+        count = parseInt(document.getElementById('cart-count').textContent, 10);
+        count++;
+        document.getElementById('cart-count').textContent = count;
+        button.textContent = "Remove from Cart";
+        button.setAttribute("onclick", "alert('Removed from Cart!')");
+        localStorage.setItem(item.id.toString(), '2');
+      }
+      else {
+        count = parseInt(document.getElementById('cart-count').textContent, 10);
+        count--;
+        document.getElementById('cart-count').textContent = count;
+        button.textContent = "Add to Cart";
+        localStorage.setItem(item.id.toString(), '1');
+      }
+      product.appendChild(button);
+    }
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
 // Script.js
+const myList = document.getElementById('product-list');
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(function(data) {
+      localStorage.setItem('array', JSON.stringify(data));
+      var array = localStorage.getItem('array')
+      var obj = JSON.parse(array);
+      obj.forEach(function(item) {
+        myList.appendChild(new ProductItem(item));
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
